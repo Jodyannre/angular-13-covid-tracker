0ef13aa5f6723dab37c3ee6126914609e6f8e25f
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2025-01-16 00:01:03 -0600

    :sparkles: feat: add routing to main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'summary',
+    pathMatch: 'full',
+  }
+  ,
+  {
+    path: 'summary',
+    loadChildren: () => import('./summary/summary.module').then((m) => m.SummaryModule),
+  },
+  {
+    path: 'live',
+    loadChildren: () => import('./live/live.module').then((m) => m.LiveModule),
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.module').then((m) => m.CountryModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
