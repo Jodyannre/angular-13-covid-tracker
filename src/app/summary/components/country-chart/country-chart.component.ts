aa6fb277fba0a71ba6425dd3732ac123889f137d
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2025-02-05 22:00:53 -0600

    :sparkles: feat: add country chart component with chart.js integration

---
+++ src/app/summary/components/country-chart/country-chart.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-country-chart',
+  templateUrl: './country-chart.component.html',
+  styleUrls: ['./country-chart.component.sass']
+})
+export class CountryChartComponent implements OnInit {
+
+  barChartData: ChartDataSets[] = [ { data: [65, 59, 80, 81, 56, 55, 40] } ]
+
+  barChartLabels: Label[] = ['USA', 'UK', 'Brazil', 'China', 'Nicaragua', 'Panama', 'Ivory Coast']
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartType: ChartType = 'bar'
+  barChartLegend = true
+  barChartPlugins = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
