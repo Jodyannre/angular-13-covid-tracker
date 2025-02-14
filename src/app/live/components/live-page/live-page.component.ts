f241e7abaeee6505666aff9add0fdb30201db918
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2025-02-13 22:31:08 -0600

    :sparkles: feat: add world map visualization for Covid-19 data in live page component with dummy data

--- src/app/live/components/live-page/live-page.component.ts
+++ src/app/live/components/live-page/live-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts/highmaps';
+import * as worldMap from '@highcharts/map-collection/custom/world.geo.json';
+
 
 @Component({
   selector: 'app-live-page',
@@ -8,6 +11,51 @@ import { Component, OnInit } from '@angular/core';
 export class LivePageComponent implements OnInit {
 
   constructor() { }
+
+  Highcharts: typeof Highcharts = Highcharts;
+  chartConstructor = 'mapChart';
+
+  chartOptions: Highcharts.Options | any = {
+    chart: {
+      map: worldMap 
+    },
+    title: 'Covid 19 world data',
+    subtitle: 'Confirmed cases live',
+    mapNavigation: {
+      enabled: true,
+      buttonOptions: {
+        alignTo: 'spacingBox'
+      }
+    },
+    Legend: {
+      enabled: true
+    },
+    colorAxis: {
+      min: 0
+    },
+    series: [{
+      type: 'map',
+      name: 'Covid 19 data',
+      states: {
+        hover: {
+          color: '#a4edba'
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        format: '{point.name}' 
+      },
+      allAreas: false,
+      data: [
+        ["fo", 0],
+        ["um", 1],
+        ["us", 2],
+        ["jp", 3],
+        ["sc", 4],
+      ]
+    }
+  ]
+  }
 
   ngOnInit(): void {
   }
